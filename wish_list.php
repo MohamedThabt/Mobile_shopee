853b6c337d3eed3a983dacac35f6913456e0bdf0
MOhamed Thabet <mohamedthabetabdelhalem_sd23@fcis.bsu.edu.eg>
2024-09-22 15:30:59 +0300

    all feature main work

--- wish_list.php
+++ wish_list.php
@@ -7,4 +7,4 @@
 
 
 <!-- check if wish list  empty or not  -->
-<?php count($product->getData('cart'))? include"view/wish_list.php" : include"view/empty.wish_list.php" ?>
+<?php count($product->getData('cart'))? include"view/wish_list.view.php" : include"view/empty.wish_list.php" ?>
